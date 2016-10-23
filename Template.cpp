3da8901c5bcd2569871c34a6e3722a593ab59695
MD.Rezaul Karim <rksazid@gmail.com>
2016-10-23 23:04:07 +0600

    My Template For Competetive Programming

---
+++ Template.cpp
@@ -0,0 +1,54 @@
+
+/**========================================================**/
+/**  In the name of Allah , most Gracious , most Merciful  **/
+/**========================================================**/
+
+                 /**=======================**/
+                 /** RKSAZID,CSE,KUET,2K14 **/
+                 /**=======================**/
+
+#include<bits/stdc++.h>
+
+/** ===================== <Sort-Cut> ===================== **/
+
+#define ll            long long int
+#define db            cout<<"Allahu Akbar"<<endl
+#define pb(a)         push_back(a)
+#define sf(t)         scanf("%d",&t)
+#define sff(a,b)      scanf("%d %d",&a,&b)
+#define sfff(a,b,c)   scanf("%d %d %d",&a,&b,&c)
+#define sfll(a)       scanf("%lld",&a)
+#define pf            printf
+#define sc            scanf
+#define uu            first
+#define vv            second
+#define mod           1000000007
+#define CAS           int t;sf(t)
+#define Cloop         for(int M = 1;M<=t;M++)
+#define Cprint        pf("Case %d: ",M)
+#define all(a)        a.begin(),a.end()
+#define ms(a,b)       memset(a, b, sizeof(a))
+#define REP(O,a,n)    for(int O = a ; O<n ; O++)
+#define CIN           ios_base::sync_with_stdio(0); cin.tie(0)
+#define pi            2*acos(0.0)
+#define insin(a,b)    asin(b/(double)a)
+#define intan(a,b)    atan(b/(double)a)
+#define Mx            100000
+using namespace std;
+
+/** ==================== <Main Code> ==================== **/
+
+int main(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
